a38e2e357e9f608d9242345eb73cc63b5d8975fd
Marcos Paulo Vilela <marcos.vilela@luizalabs.com.br>
2019-11-19 17:47:21 -0300

    minimal rest server

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
+const logger = {
+    info: console.log,
+    error: console.error,
+}
+
 const Koa = require('koa')
-const logger = console
+const KoaRouter = require('koa-router');
+const KoaBodyParser = require('koa-bodyparser');
 
 const koaApp = new Koa()
+const router = new KoaRouter()
+
+router.get('/', ctx => ctx.body = 'Generic API!!')
+
+koaApp
+.use(KoaBodyParser())
+.use(router.routes())
+.use(router.allowedMethods())
+
 const httpServer = koaApp.listen(process.env.PORT || 3000, _ => {
-    logger.log(`server running at ${httpServer.address().address}:${httpServer.address().port}`)
+    logger.info(`server running at ${httpServer.address().address}:${httpServer.address().port}`)
 })
